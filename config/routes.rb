71d987dfa23f93f90abbea26eb29316d6820f32b
a2316ys <130154894+a2316ys@users.noreply.github.com>
2023-06-02 12:09:55 +0000

    Finish ch12

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'users/new'
   # get 'static_pages/home'
   # get 'static_pages/help'
@@ -19,6 +21,7 @@ Rails.application.routes.draw do
   end
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
   get '/microposts', to: 'static_pages#home'
